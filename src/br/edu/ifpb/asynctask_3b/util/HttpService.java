19ce318e1cc6afd7ae098031727ac50a49d19e91
natan1503 <natan1503@hotmail.com>
2015-12-23 22:35:35 -0300

    Commit final
    Final

--- src/br/edu/ifpb/asynctask_3b/util/HttpService.java
+++ src/br/edu/ifpb/asynctask_3b/util/HttpService.java
@@ -19,7 +19,9 @@ public class HttpService {
 
     // IP da máquina onde se encontra o servidor.
     private static final String URL_CONTEXT = "http://192.168.0.168:8080/rest-servlet-service/";
-
+    
+    private static final String POST_PARAMS = "nomeEditText=fulano";
+    
     public static HttpURLConnection sendGetRequest(String service)
             throws MalformedURLException, IOException{
 
@@ -28,14 +30,44 @@ public class HttpService {
         URL url = new URL(URL_CONTEXT + service);
 
         connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+        connection.setRequestMethod("POST");
         connection.connect();
 
         return connection;
     }
 
     public void sendJsonPostRequest(String service, JSONObject json) {
-        //TODO: Implementar conexão com o Servidor REST.
+    	URL obj = new URL(URL_CONTEXT);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+ 
+        // For POST only - START
+        con.setDoOutput(true);
+        OutputStream os = con.getOutputStream();
+        os.write(POST_PARAMS.getBytes());
+        os.flush();
+        os.close();
+        // For POST only - END
+ 
+        int responseCode = con.getResponseCode();
+        System.out.println("POST Response Code :: " + responseCode);
+ 
+        if (responseCode == HttpURLConnection.HTTP_OK) { //success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+ 
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+ 
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST request not worked");
+        }
     }
 
     public static String getHttpContent(HttpURLConnection connection) {
